927c066eeac0b87f12ed3642f485301c4fe09638
Rodrigo Barbosa <rodrigosb_01@hotmail.com>
2026-07-07 02:22:38 -0300

    Alteração

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from interface.app import App
 from interface.sidebar import Sidebar
 from interface.dashboard import Dashboard
-from interface.app import App
 
 if __name__ == "__main__":
     app = App()
